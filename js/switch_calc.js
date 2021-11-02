5388613c865efe59e7c2e5f21390503b16d0fd8c
karaell <elenakara2@gmail.com>
2021-11-02 14:47:40 +0300

    change isvalidoperand

--- js/switch_calc.js
+++ js/switch_calc.js
@@ -1,6 +1,6 @@
 function calc (a, b, operator) {
         
-    const isValidOperand = ( (typeof a === 'number') && (typeof b === 'number')  && isFinite(a) && isFinite(b) )
+    const isValidOperand = ( (typeof a === 'number') && (typeof b === 'number')  && b != 0 )
     
     if ( isValidOperand ) {
     switch (operator) {
@@ -24,8 +24,4 @@ function calc (a, b, operator) {
     }
 } 
 
-console.log( calc(1, NaN, "/")); // Error
-console.log( calc(1, 0, "/")); // Infinity ??
-console.log( calc(1, 2, "=")); // Unknown operation
-console.log( calc(1, 0, null)); // Unknown operation
-console.log( calc(null, 2, "+")) // Error
+console.log( calc());
